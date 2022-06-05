68a8c608ef2d53971023aa1e8303f3216844f98f
GagneeshGitHub <76528821+GagneeshGitHub@users.noreply.github.com>
2022-06-05 14:59:32 +0530

    Update MidComp.js

--- src/midComp/MidComp.js
+++ src/midComp/MidComp.js
@@ -8,7 +8,7 @@ export default function MidComp() {
     return (
         <div className='main-division'>
             <div className='inner-first'>
-                <img id='award' src={award} alt='award-photo'/>
+                <img id='award' src={award} alt='award'/>
                 <div className='right-div'>
                     <h3> C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h3>
                     <ul>
@@ -16,7 +16,7 @@ export default function MidComp() {
                         <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
                     </ul>
                     <br />
-                    <img src={awardImg} alt='taking-award-8mage'/>
+                    <img src={awardImg} alt='taking-award'/>
                     <p>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
                 </div>
             </div>
