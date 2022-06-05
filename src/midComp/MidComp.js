5935058f27fc8fde79aa95b3340d060378e666c2
GagneeshGitHub <76528821+GagneeshGitHub@users.noreply.github.com>
2022-06-05 14:53:26 +0530

    Update MidComp.js

--- src/midComp/MidComp.js
+++ src/midComp/MidComp.js
@@ -8,7 +8,7 @@ export default function MidComp() {
     return (
         <div className='main-division'>
             <div className='inner-first'>
-                <img id='award' src={award} />
+                <img id='award' src={award} alt='award-photo'/>
                 <div className='right-div'>
                     <h3> C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h3>
                     <ul>
@@ -16,13 +16,13 @@ export default function MidComp() {
                         <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
                     </ul>
                     <br />
-                    <img src={awardImg} />
+                    <img src={awardImg} alt='taking-award-8mage'/>
                     <p>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
                 </div>
             </div>
             <div className='inner-second'>
                 <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
-                <img id='pumpimg' src={partsImg} />
+                <img id='pumpimg' src={partsImg} alt='pump-elements'/>
                 <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</p>
                 <hr />
                 <h3>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h3>
