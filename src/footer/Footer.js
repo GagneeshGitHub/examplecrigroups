480a2bb0aa923b0813b83aac308e575a9e1e7bc1
GagneeshGitHub <76528821+GagneeshGitHub@users.noreply.github.com>
2022-06-05 14:48:52 +0530

    empty img delete

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -5,15 +5,12 @@ export default function Footer() {
   return (
     <div className='footer'>
         <div className='con-numb'>
-            <img />
             <p>Toll Free 1800 200 1734</p>
         </div>
         <div className='con-face'>
-            <img />
             <p><a href='https://www.facebook.com/cripumps'>www.facebook.com/cripumps</a></p>
         </div>
         <div className='con-web'>
-            <img />
             <p><a href='https://www.crigroups.com/'>www.crigroups.com</a></p>
         </div>
     </div>
